27be08b2e2ea6df5eeabdfb3f61d66f7fce30af5
Carla Escobar <carlaprimola@outlook.es>
2024-04-25 13:55:08 +0200

    changes for render in general package.json

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 //ejecutar aplicacion
 import app from './app.js';
 import connectDB from './db.js';
+import path from 'path';
 
 connectDB();
 app.listen(3000);
 console.log('✨ Servidor conectado', 3000);
 
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
